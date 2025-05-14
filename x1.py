d71cf3eda0c690bda95922cfcae7b671b03c1d23
Vanshika <Vanshikaagarwal278@gmail.com>
2025-05-14 15:20:15 +0530

    made changes

--- x1.py
+++ x1.py
@@ -1,1 +1,7 @@
-print ("hello world")
+# Get user input
+name = input("Please enter your name: ")
+email = input("Please enter your email: ")
+
+# Print the collected information
+print(f"Name: {name}")
+print(f"Email: {email}")
